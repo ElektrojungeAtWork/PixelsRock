b512616fee6a64d54092b2c2ce709d52cfcac017
Benjamin Scholtysik (Reimold) <bereimol@microsoft.com>
2018-03-21 00:07:11 -0700

    Add a simple instrumented test

--- app/src/androidTest/java/de/elektrojunge/android/pixelsrock/SomeInstrumentedTestCase.kt
+++ app/src/androidTest/java/de/elektrojunge/android/pixelsrock/SomeInstrumentedTestCase.kt
@@ -1,12 +1,13 @@
 package de.elektrojunge.android.pixelsrock
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
-
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,11 +15,22 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class SomeInstrumentedTestCase {
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
         assertEquals("de.elektrojunge.android.pixelsrock", appContext.packageName)
     }
+
+    @Test
+    fun initPixelWorks() {
+        val appContext = InstrumentationRegistry.getTargetContext()
+        val sut = Pixel(appContext, Color.LTGRAY, 100)
+        assertNotNull(sut)
+        assertNotNull(sut.pixelView)
+        val background : ColorDrawable = sut.pixelView?.background as ColorDrawable
+        assertEquals(Color.LTGRAY, background.color)
+    }
+
 }
