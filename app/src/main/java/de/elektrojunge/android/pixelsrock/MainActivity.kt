db9f77f1f3541cf3f2456bf61cb0e6ef1a5a85f1
Benjamin Scholtysik (Reimold) <bereimol@microsoft.com>
2018-03-22 00:19:56 -0700

    Add appcenter SDK

--- app/src/main/java/de/elektrojunge/android/pixelsrock/MainActivity.kt
+++ app/src/main/java/de/elektrojunge/android/pixelsrock/MainActivity.kt
@@ -8,6 +8,11 @@ import android.view.MotionEvent
 import android.view.View
 import android.widget.Button
 import android.widget.GridLayout
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.analytics.Analytics.trackEvent
+import com.microsoft.appcenter.crashes.Crashes
+import com.microsoft.appcenter.distribute.Distribute
 import eltos.simpledialogfragment.SimpleDialog
 import eltos.simpledialogfragment.SimpleDialog.OnDialogResultListener.BUTTON_POSITIVE
 import eltos.simpledialogfragment.color.SimpleColorDialog
@@ -15,13 +20,18 @@ import eltos.simpledialogfragment.color.SimpleColorDialog
 
 class MainActivity : AppCompatActivity(), SimpleDialog.OnDialogResultListener {
 
-    private var pickerButton : Button? = null
-    private var clearButton : Button? = null
-    private var selectedColor : Int = Color.MAGENTA
+    private var pickerButton: Button? = null
+    private var clearButton: Button? = null
+    private var selectedColor: Int = Color.MAGENTA
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize App Center SDK
+        AppCenter.start(application, "55efc107-ee0c-40e4-a4e7-e4f283b59b2e",
+                Analytics::class.java, Crashes::class.java, Distribute::class.java)
+
 
         val grid = findViewById<GridLayout>(R.id.pixelGrid)
         val pixelCount = grid.columnCount * grid.columnCount
@@ -52,29 +62,33 @@ class MainActivity : AppCompatActivity(), SimpleDialog.OnDialogResultListener {
 
         this.pickerButton = findViewById<Button>(R.id.colorPickerButton)
         this.pickerButton?.setBackgroundColor(this.selectedColor)
-        this.pickerButton?.setOnClickListener{
+        this.pickerButton?.setOnClickListener {
             SimpleColorDialog.build()
                     .title(R.string.colorPickerButton)
                     .colorPreset(this.selectedColor)
                     .allowCustom(true)
                     .show(this, getString(R.string.tag_color_picker_dialog))
+            trackEvent("button_color_picker_hit")
         }
 
         this.clearButton = findViewById<Button>(R.id.clearButton)
         this.clearButton?.setBackgroundColor(Color.DKGRAY)
-        this.clearButton?.setOnClickListener{
-            for (pixel : Pixel in pixels) {
+        this.clearButton?.setOnClickListener {
+            for (pixel: Pixel in pixels) {
                 pixel.color = Color.LTGRAY
             }
         }
 
     }
 
-    // Color Picker onnResult callback
+    // Color Picker onResult callback
     override fun onResult(dialogTag: String, which: Int, extras: Bundle): Boolean {
         if (which == BUTTON_POSITIVE) {
             this.selectedColor = extras.getInt(SimpleColorDialog.COLOR)
             this.pickerButton?.setBackgroundColor(this.selectedColor)
+            val properties: Map<String, String> = hashMapOf("color" to
+                    Color.valueOf(this.selectedColor).toString())
+            trackEvent("color_picker_color_selected", properties)
             return true
         }
 
