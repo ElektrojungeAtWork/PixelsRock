f744b97edf77490d47e645cd24fc30961e05159b
Benjamin Scholtysik (Reimold) <bereimol@microsoft.com>
2018-03-22 00:59:24 -0700

    add appcenter test

--- app/src/androidTest/java/de/elektrojunge/android/pixelsrock/SomeEspressoTests.kt
+++ app/src/androidTest/java/de/elektrojunge/android/pixelsrock/SomeEspressoTests.kt
@@ -6,10 +6,12 @@ import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
 import android.support.test.espresso.matcher.ViewMatchers.withText
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
+import com.microsoft.appcenter.espresso.Factory
+import com.microsoft.appcenter.espresso.ReportHelper
+import org.junit.After
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
 
 @RunWith(AndroidJUnit4::class)
 class SomeEspressoTests {
@@ -17,10 +19,18 @@ class SomeEspressoTests {
     @Rule @JvmField
      var mActivityRule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
 
+    @Rule @JvmField
+    var reportHelper: ReportHelper = Factory.getReportHelper()
+
     @Test
     fun buttonsAreVisible() {
         onView(withText("Pick a color!")).check(matches(isDisplayed()))
         onView(withText("Clear!")).check(matches(isDisplayed()))
+        reportHelper.label("Both buttons have been verified!")
+    }
 
+    @After
+    fun tearDown() {
+        reportHelper.label("Finished testing buttons")
     }
 }
